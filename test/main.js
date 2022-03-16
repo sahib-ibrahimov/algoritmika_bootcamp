57fe10bdbb50fa2412ccf47215e0a936f88c725b
sahib-ibrahimov <sahib.ibrahimov1998@gmail.com>
2022-03-16 18:53:45 +0400

    rekursiya

--- test/main.js
+++ test/main.js
@@ -1,9 +1,11 @@
-console.log('salam');
-console.clear();
+const F_LOG = 0;
+const F_WARN = 1;
+const F_ERROR = 2;
 
-let a = 5;
-const b = 0;
-
-let c = a / b;
-
-console.log(c);
+function print(msg, stat=F_LOG) {
+  switch (stat) {
+    case F_LOG: console.log(msg);   break;
+    case F_WARN: console.warn(msg);  break;
+    default: console.error(msg);
+  }
+}
